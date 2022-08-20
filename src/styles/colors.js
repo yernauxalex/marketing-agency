577fd8fc6a6c2b53265f91cbb7c2db50e42f3b2d
Alexis <yernauxalex@gmail.com>
2022-08-20 08:50:19 +0200

    Add bootsrap CDN + css values

---
+++ src/styles/colors.js
@@ -0,0 +1,8 @@
+const colors = {
+    primary: '#151515',
+    secondary: '#444444',
+    background: '#ffffff',
+    backgroundSecondary: '#ededed',
+    button: '#5134ff'
+};
+export default colors;
---
+++ src/styles/fonts.js
@@ -0,0 +1,16 @@
+const fonts = {
+    header: {
+        title: '1.5rem',
+        text: '1.2rem',
+        button: '1.2rem'
+    },
+    h1: '2.5rem',
+    h2: '2rem',
+    button: '1.5rem',
+    text: '1rem',
+    footer: {
+        title: '1.5rem',
+        text: '1rem'
+    }
+};
+export default fonts;
